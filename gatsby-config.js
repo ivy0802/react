69029b9b852552e87d3d25e81b67ddb7d7f7229d
Zhao Chen <zhaochen@Zhaos-MacBook-Pro.local>
2018-11-01 13:22:29 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Sipas',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
